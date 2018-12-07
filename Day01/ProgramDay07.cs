34d04e6e9159af3911ff9eee6d1e74a47ec30fa0
Gerco Brandwijk <g.brandwijk@gobright.eu>
2018-12-07 21:32:07 +0100

    Part one day 7

---
+++ Day01/ProgramDay07.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class ProgramDay07
+    {
+        public static void Run()
+        {
+            Console.WriteLine("ProgramDay06");
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            Console.WriteLine("Part one");
+            watch.Restart();
+            partOne();
+            watch.Stop();
+            Console.WriteLine($"Done in: {watch.Elapsed.TotalMilliseconds}ms");
+
+            //Console.WriteLine("Part two");
+            //watch.Restart();
+            //partTwo();
+            //watch.Stop();
+            //Console.WriteLine($"Done in: {watch.Elapsed.TotalMilliseconds}ms");
+
+            Console.ReadLine();
+        }
+
+        static void partOne()
+        {
+            partOne(inputValuesTest);
+            partOne(realInputValues);
+        }
+
+        static void partOne(string[] values)
+        {
+            List<Step> allSteps = new List<Step>();
+
+            foreach (string value in values)
+            {
+                Step step = allSteps.SingleOrDefault(x => x.Name == value[5]);
+
+                if (step == null)
+                {
+                    step = new Step(value[5]);
+
+                    allSteps.Add(step);
+                }
+
+                Step nextStep = allSteps.SingleOrDefault(x => x.Name == value[36]);
+
+                if (nextStep == null)
+                {
+                    nextStep = new Step(value[36]);
+
+                    allSteps.Add(nextStep);
+                }
+
+                step.Next.Add(nextStep);
+
+                nextStep.Conditions.Add(step);
+            }
+
+            List<Step> stepsToExecute = allSteps.Where(x => !x.Conditions.Any()).ToList();
+
+            String result = "";
+
+            do
+            {
+                Step step = stepsToExecute.OrderBy(x => x.Name).First();
+                step.Done = true;
+
+                result += step;
+
+                stepsToExecute.Remove(step);
+
+                foreach (Step nextStep in step.Next)
+                {
+                    nextStep.Conditions.Remove(step);
+
+                    if (!nextStep.Conditions.Any())
+                        stepsToExecute.Add(nextStep);
+                }
+            }
+            while (stepsToExecute.Any());
+
+            Console.WriteLine("Execution order: " + result);
+        }
+
+        public class Step
+        {
+            public List<Step> Next { get; set; }
+
+            public List<Step> Conditions { get; set; }
+
+            public char Name { get; set; }
+
+            public bool Done { get; set; }
+
+            public Step(char name)
+            {
+                this.Name = name;
+                this.Next = new List<Step>();
+                this.Conditions = new List<Step>();
+            }
+
+            public override string ToString()
+            {
+                return this.Name.ToString();
+            }
+        }
+
+        static string[] inputValuesTest = new string[]
+        {
+            "Step C must be finished before step A can begin.",
+            "Step C must be finished before step F can begin.",
+            "Step A must be finished before step B can begin.",
+            "Step A must be finished before step D can begin.",
+            "Step B must be finished before step E can begin.",
+            "Step D must be finished before step E can begin.",
+            "Step F must be finished before step E can begin."
+        };
+
+        static string[] realInputValues = new string[]
+        {
+            "Step X must be finished before step C can begin.",
+            "Step C must be finished before step G can begin.",
+            "Step F must be finished before step G can begin.",
+            "Step U must be finished before step Y can begin.",
+            "Step O must be finished before step S can begin.",
+            "Step D must be finished before step N can begin.",
+            "Step M must be finished before step H can begin.",
+            "Step J must be finished before step Q can begin.",
+            "Step G must be finished before step R can begin.",
+            "Step I must be finished before step N can begin.",
+            "Step R must be finished before step K can begin.",
+            "Step A must be finished before step Z can begin.",
+            "Step Y must be finished before step L can begin.",
+            "Step H must be finished before step P can begin.",
+            "Step K must be finished before step S can begin.",
+            "Step Z must be finished before step P can begin.",
+            "Step T must be finished before step S can begin.",
+            "Step N must be finished before step P can begin.",
+            "Step E must be finished before step S can begin.",
+            "Step S must be finished before step W can begin.",
+            "Step W must be finished before step V can begin.",
+            "Step L must be finished before step V can begin.",
+            "Step P must be finished before step B can begin.",
+            "Step Q must be finished before step V can begin.",
+            "Step B must be finished before step V can begin.",
+            "Step P must be finished before step Q can begin.",
+            "Step S must be finished before step V can begin.",
+            "Step C must be finished before step Q can begin.",
+            "Step I must be finished before step H can begin.",
+            "Step A must be finished before step E can begin.",
+            "Step H must be finished before step Q can begin.",
+            "Step G must be finished before step V can begin.",
+            "Step N must be finished before step L can begin.",
+            "Step R must be finished before step Q can begin.",
+            "Step W must be finished before step L can begin.",
+            "Step X must be finished before step L can begin.",
+            "Step X must be finished before step J can begin.",
+            "Step W must be finished before step P can begin.",
+            "Step U must be finished before step B can begin.",
+            "Step P must be finished before step V can begin.",
+            "Step O must be finished before step P can begin.",
+            "Step W must be finished before step Q can begin.",
+            "Step S must be finished before step Q can begin.",
+            "Step U must be finished before step Z can begin.",
+            "Step Z must be finished before step T can begin.",
+            "Step M must be finished before step T can begin.",
+            "Step A must be finished before step P can begin.",
+            "Step Z must be finished before step B can begin.",
+            "Step N must be finished before step S can begin.",
+            "Step H must be finished before step N can begin.",
+            "Step J must be finished before step E can begin.",
+            "Step M must be finished before step J can begin.",
+            "Step R must be finished before step A can begin.",
+            "Step A must be finished before step Y can begin.",
+            "Step F must be finished before step V can begin.",
+            "Step L must be finished before step P can begin.",
+            "Step K must be finished before step L can begin.",
+            "Step F must be finished before step P can begin.",
+            "Step G must be finished before step L can begin.",
+            "Step I must be finished before step Q can begin.",
+            "Step C must be finished before step L can begin.",
+            "Step I must be finished before step Y can begin.",
+            "Step G must be finished before step B can begin.",
+            "Step H must be finished before step L can begin.",
+            "Step X must be finished before step U can begin.",
+            "Step I must be finished before step K can begin.",
+            "Step R must be finished before step N can begin.",
+            "Step I must be finished before step L can begin.",
+            "Step M must be finished before step I can begin.",
+            "Step K must be finished before step V can begin.",
+            "Step G must be finished before step E can begin.",
+            "Step F must be finished before step B can begin.",
+            "Step O must be finished before step Y can begin.",
+            "Step Y must be finished before step Q can begin.",
+            "Step F must be finished before step K can begin.",
+            "Step N must be finished before step W can begin.",
+            "Step O must be finished before step R can begin.",
+            "Step N must be finished before step E can begin.",
+            "Step M must be finished before step V can begin.",
+            "Step H must be finished before step T can begin.",
+            "Step Y must be finished before step T can begin.",
+            "Step F must be finished before step J can begin.",
+            "Step F must be finished before step O can begin.",
+            "Step W must be finished before step B can begin.",
+            "Step T must be finished before step E can begin.",
+            "Step T must be finished before step P can begin.",
+            "Step F must be finished before step M can begin.",
+            "Step U must be finished before step I can begin.",
+            "Step H must be finished before step S can begin.",
+            "Step S must be finished before step P can begin.",
+            "Step T must be finished before step W can begin.",
+            "Step A must be finished before step N can begin.",
+            "Step O must be finished before step N can begin.",
+            "Step L must be finished before step B can begin.",
+            "Step U must be finished before step K can begin.",
+            "Step Z must be finished before step W can begin.",
+            "Step X must be finished before step D can begin.",
+            "Step Z must be finished before step L can begin.",
+            "Step I must be finished before step T can begin.",
+            "Step O must be finished before step W can begin.",
+            "Step I must be finished before step B can begin."
+        };
+    }
+}
