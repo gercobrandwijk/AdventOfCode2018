feb4d6876212b4b1a0cd0afe3ab5519dcc6fac05
Gerco Brandwijk <g.brandwijk@gobright.eu>
2018-12-18 22:29:49 +0100

    Answer for Day14, in only 3 minutes...

--- AdventOfCode/ProgramDay14.cs
+++ AdventOfCode/ProgramDay14.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 
@@ -12,11 +13,11 @@ namespace AdventOfCode
 
             Stopwatch watch = Stopwatch.StartNew();
 
-            //Console.WriteLine("Part one");
-            //watch.Restart();
-            //partOne();
-            //watch.Stop();
-            //Console.WriteLine($"Done in: {watch.Elapsed.TotalMilliseconds}ms");
+            Console.WriteLine("Part one");
+            watch.Restart();
+            partOne();
+            watch.Stop();
+            Console.WriteLine($"Done in: {watch.Elapsed.TotalMilliseconds}ms");
 
             Console.WriteLine("Part two");
             watch.Restart();
@@ -25,167 +26,36 @@ namespace AdventOfCode
             Console.WriteLine($"Done in: {watch.Elapsed.TotalMilliseconds}ms");
         }
 
-        //static void partOne()
-        //{
-        //    executePartOne(inputTest.ToList(), 5, "0124515891");
-        //    executePartOne(inputTest.ToList(), 9, "5158916779");
-        //    executePartOne(inputTest.ToList(), 18, "9251071085");
-        //    executePartOne(inputTest.ToList(), 2018, "5941429882");
-        //    executePartOne(inputTest.ToList(), 077201);
-        //}
-
-        //static void executePartOne(List<int> values, int rounds, string sequence = null)
-        //{
-        //    Elf elf1 = new Elf() { Position = 0, Value = values[0] };
-        //    Elf elf2 = new Elf() { Position = 1, Value = values[1] };
-
-        //    while (values.Count < rounds + 10)
-        //    {
-        //        int newValue = elf1.Value + elf2.Value;
-
-        //        string newValueString = newValue.ToString();
-
-        //        foreach (char newValuePart in newValueString)
-        //        {
-        //            values.Add((int)newValuePart - 48);
-        //        }
-
-        //        elf1.Position = (elf1.Position + 1 + elf1.Value) % values.Count;
-        //        elf1.Value = values[elf1.Position];
-
-        //        elf2.Position = (elf2.Position + 1 + elf2.Value) % values.Count;
-        //        elf2.Value = values[elf2.Position];
-
-        //        //for (int j = 0; j < values.Count; j++)
-        //        //{
-        //        //    int value = values[j];
-
-        //        //    if (elf1.Position == j)
-        //        //    {
-        //        //        Console.Write("(" + value + ") ");
-        //        //    }
-        //        //    else if (elf2.Position == j)
-        //        //    {
-        //        //        Console.Write("[" + value + "] ");
-        //        //    }
-        //        //    else
-        //        //    {
-        //        //        Console.Write(" " + value + "  ");
-        //        //    }
-        //        //}
-
-        //        //Console.WriteLine();
-        //    }
-
-        //    string foundResult = string.Join("", values).Substring(rounds, 10);
-
-        //    if (sequence != null)
-        //        Console.WriteLine("Output: " + foundResult + ", equal = " + (foundResult == sequence));
-        //    else
-        //        Console.WriteLine("Output: " + foundResult);
-        //}
-
-        static void partTwo()
+        static void partOne()
         {
-            executePartTwo(baseInput, "01245", 5);
-            executePartTwo(baseInput, "51589", 9);
-            executePartTwo(baseInput, "92510", 18);
-            executePartTwo(baseInput, "59414", 2018);
-            executePartTwo(baseInput, "077201");
+            executePartOne(new List<int>() { 3, 7 }, 5, "0124515891");
+            executePartOne(new List<int>() { 3, 7 }, 9, "5158916779");
+            executePartOne(new List<int>() { 3, 7 }, 18, "9251071085");
+            executePartOne(new List<int>() { 3, 7 }, 2018, "5941429882");
+            executePartOne(new List<int>() { 3, 7 }, 077201);
         }
 
-        static void executePartTwo(string valuesString, string sequence, int? rounds = null)
+        static void executePartOne(List<int> values, int rounds, string sequence = null)
         {
-            Elf elf1 = new Elf() { Position = 0, Value = valuesString[0] };
-            Elf elf2 = new Elf() { Position = 1, Value = valuesString[1] };
+            Elf elf1 = new Elf() { Position = 0, Value = values[0] };
+            Elf elf2 = new Elf() { Position = 1, Value = values[1] };
 
-            int wishedResultLength = sequence.Length;
-
-            bool validLength = false;
-            StringBuilder values = new StringBuilder(valuesString);
-            StringBuilder valueToCompare = new StringBuilder(valuesString);
-            int skip = 0;
-
-            while (true)
+            while (values.Count < rounds + 10)
             {
-                int newValue = elf1.ValueInt + elf2.ValueInt;
+                int newValue = elf1.Value + elf2.Value;
 
                 string newValueString = newValue.ToString();
 
                 foreach (char newValuePart in newValueString)
                 {
-                    values.Append(newValuePart);
-
-                    if (validLength || valueToCompare.Length >= wishedResultLength)
-                    {
-                        validLength = true;
-
-                        valueToCompare.Remove(0, 1);
-                        valueToCompare.Append(newValuePart);
-                    }
-                    else
-                    {
-                        valueToCompare.Append(newValuePart);
-                    }
+                    values.Add((int)newValuePart - 48);
                 }
 
-                int elf1NewPosition = elf1.Position + 1 + elf1.ValueInt;
-
-                while (elf1NewPosition >= values.Length)
-                    elf1NewPosition -= values.Length;
-
-                elf1.Position = elf1NewPosition;
+                elf1.Position = (elf1.Position + 1 + elf1.Value) % values.Count;
                 elf1.Value = values[elf1.Position];
 
-                int elf2NewPosition = elf2.Position + 1 + elf2.ValueInt;
-
-                while (elf2NewPosition >= values.Length)
-                    elf2NewPosition -= values.Length;
-
-                elf2.Position = elf2NewPosition;
+                elf2.Position = (elf2.Position + 1 + elf2.Value) % values.Count;
                 elf2.Value = values[elf2.Position];
-
-                if (validLength)
-                {
-                    if (skip == 0)
-                    {
-                        string valueToCompareString = valueToCompare.ToString();
-
-                        int indexOfZero = valueToCompareString.IndexOf(sequence[0]);
-
-                        if (indexOfZero >= 0)
-                        {
-                            int indexOf1 = valueToCompareString.IndexOf(sequence[1]);
-
-                            if (indexOf1 >= 0)
-                            {
-                                int indexOf2 = valueToCompareString.IndexOf(sequence[2]);
-
-                                if (indexOf2 >= 0)
-                                {
-                                    if (sequence.Equals(valueToCompareString))
-                                        break;
-                                }
-                                else
-                                {
-                                    skip += 1;
-                                }
-                            }
-                            else
-                            {
-                                skip += 2;
-                            }
-                        }
-                        else
-                        {
-                            skip += 3;
-                        }
-                    }
-                    else
-                    {
-                        skip--;
-                    }
-                }
 
                 //for (int j = 0; j < values.Count; j++)
                 //{
@@ -208,7 +78,103 @@ namespace AdventOfCode
                 //Console.WriteLine();
             }
 
-            int foundResult = values.Length - wishedResultLength;
+            string foundResult = string.Join("", values).Substring(rounds, 10);
+
+            if (sequence != null)
+                Console.WriteLine("Output: " + foundResult + ", equal = " + (foundResult == sequence));
+            else
+                Console.WriteLine("Output: " + foundResult);
+        }
+
+        static void partTwo()
+        {
+            executePartTwo("37", "01245", 5);
+            executePartTwo("37", "51589", 9);
+            executePartTwo("37", "515891", 9);
+            executePartTwo("37", "92510", 18);
+            executePartTwo("37", "59414", 2018);
+            executePartTwo("37", "077201");
+        }
+
+        static void executePartTwo(string valuesString, string sequence, int? rounds = null)
+        {
+            ElfPart2 elf1 = new ElfPart2() { Position = 0, Value = valuesString[0] };
+            ElfPart2 elf2 = new ElfPart2() { Position = 1, Value = valuesString[1] };
+
+            int sequenceLength = sequence.Length;
+            int sequenceNumber = int.Parse(sequence);
+
+            bool validLength = false;
+            StringBuilder values = new StringBuilder(valuesString);
+            StringBuilder valueToCompare = new StringBuilder(valuesString);
+
+            string valueToCompareString;
+
+            while (true)
+            {
+                int newValue = elf1.ValueInt + elf2.ValueInt;
+
+                string newValueString = newValue.ToString();
+
+                foreach (char newValuePart in newValueString)
+                {
+                    values.Append(newValuePart);
+
+                    valueToCompare.Append(newValuePart);
+                }
+
+                if (!validLength)
+                {
+                    if (valueToCompare.Length >= sequenceLength)
+                        validLength = true;
+                }
+                else
+                {
+                    if (valueToCompare.Length > sequenceLength + 2)
+                        valueToCompare.Remove(0, 1);
+
+                    if (valueToCompare.Length > sequenceLength + 1)
+                        valueToCompare.Remove(0, 1);
+                }
+
+                int elf1NewPosition = elf1.Position + 1 + elf1.ValueInt;
+
+                if (elf1NewPosition >= values.Length)
+                    while (elf1NewPosition >= values.Length)
+                        elf1NewPosition -= values.Length;
+
+                elf1.Position = elf1NewPosition;
+                elf1.Value = values[elf1.Position];
+
+                int elf2NewPosition = elf2.Position + 1 + elf2.ValueInt;
+
+                if (elf2NewPosition >= values.Length)
+                    while (elf2NewPosition >= values.Length)
+                        elf2NewPosition -= values.Length;
+
+                elf2.Position = elf2NewPosition;
+                elf2.Value = values[elf2.Position];
+
+                if (validLength)
+                {
+                    valueToCompareString = valueToCompare.ToString();
+
+                    if (valueToCompareString[0] == sequence[0] || valueToCompareString[1] == sequence[0])
+                    {
+                        int compareInt1 = int.Parse(valueToCompareString.Substring(0, valueToCompareString.Length - 1));
+
+                        if (compareInt1 == sequenceNumber)
+                            break;
+
+                        int compareInt2 = int.Parse(valueToCompareString.Substring(1));
+
+                        if (compareInt2 == sequenceNumber)
+                            break;
+                    }
+                }
+            }
+
+            int foundResult = values.ToString().IndexOf(sequence);
 
             if (rounds != null)
                 Console.WriteLine("Output: " + foundResult + ", equal = " + (foundResult == rounds));
@@ -220,7 +186,14 @@ namespace AdventOfCode
         {
             public int Position { get; set; }
 
-            public char value;
+            public int Value { get; set; }
+        }
+
+        public class ElfPart2
+        {
+            public int Position { get; set; }
+
+            private char value;
             public char Value
             {
                 get
@@ -237,7 +210,5 @@ namespace AdventOfCode
 
             public int ValueInt { get; set; }
         }
-
-        static string baseInput = "37";
     }
 }
